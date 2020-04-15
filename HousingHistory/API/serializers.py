f5e6405a858abf5a55f25b2f84d015fbbe1daae5
DorianLcq <lacluque.d@gmail.com>
2020-04-15 12:58:20 +0100

    update 15/04/2020

--- HousingHistory/API/serializers.py
+++ HousingHistory/API/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers 
 from API.models import HousingHistory
+import re
+from ukpostcodeutils import validation
  
  
 class HousingHistorySerializer(serializers.ModelSerializer):
@@ -15,3 +17,13 @@ class HousingHistorySerializer(serializers.ModelSerializer):
                 'country',
                 'movingDate']
 
+    def validate_postcode(self,value):
+        postcode = re.findall("^[A-Za-z0-9 ]{5,8}$",value)
+        if postcode:
+            postcode = value.upper().replace(" ","")        # Convert to uppercase and remove the spaces
+            if validation.is_valid_postcode(postcode):      # Check if the postcode exists, to use it you need to import  from ukpostcodeutils import validation
+                return postcode
+            else:
+                raise serializers.ValidationError('This postcode does not exist')
+        else:
+            raise serializers.ValidationError("Remember, the length of the postcode must be between 5-8 and it can not contain special character(/?*...) ")
